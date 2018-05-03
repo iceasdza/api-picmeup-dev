609af0b6b02a483ae3ec834b1490bdf997fc52e0
iceasdza <ice_ro.patis@hotmail.com>
2018-05-04 00:45:56 +0700

    [Add]API for DELETE FILE and fix something

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const multer  = require('multer')
 const mongoose = require('mongoose')
 const Places = require('./model/places')
 const bodyParser = require('body-parser')
+const fs = require('fs')
 
 // used module
 const app = express()
@@ -29,13 +30,24 @@ app.listen(3030, () => console.log('Example app listening on port 3030!'))
 
 //uload single file
 app.post('/api/uploadSingleFile', upload.single('img'), (req,res)=>{
-      
     res.send(console.log("single upload says : ",req)) 
  })
  //upload multiple file 
  app.post('/api/uploadMultipleFile', upload.array('img', 12), (req,res)=>{
      res.send(console.log("multiple upload says : ",req))
  })
+
+ app.get('/api/deleteImage/:_id',(req,res)=>{
+    const id = req.params._id;
+     res.send(console.log(id))
+    fs.unlink("static/images/places/"+id,function (err){
+        if(err){
+            throw err
+            res.send("can")
+        }
+    })
+ })
+
 
 
  app.post('/api/testData',(req,res) => {
