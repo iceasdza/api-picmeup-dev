a94de6dcd26917c6b5f5b9d2b5dd7a4e290d909d
iceasdza <ice_ro.patis@hotmail.com>
2018-05-04 22:24:29 +0700

    [ADD] ADD METHOD FOR GET AND DELETE

--- model/places.js
+++ model/places.js
@@ -48,3 +48,18 @@ const Places = module.exports = mongoose.model('Place',Place)
 module.exports.addPlace = (places,callback) =>{
     Places.create(places,callback);
 }
+//find one
+module.exports.getPlaceInfoFromID = (id,callback,limit)=>{
+    const query = {_id:id}
+    Places.find({_id:query},callback).limit(limit)
+}
+//find all
+module.exports.getPlaceInfo = (callback,limit)=>{
+    Places.find(callback).limit(limit)
+}
+
+//remove from Id
+module.exports.DeletePlaceFromId = (id,callback)=>{
+    const query = {_id:id}
+    Places.remove(query,callback)
+}
